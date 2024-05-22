46bda82243fd696f266135e08abeed78ff004a0a
Prajkta <gudleprajkta2001@gmail.com>
2024-05-22 14:21:50 +0530

    first commit

---
+++ First.js
@@ -0,0 +1,17 @@
+//let & const has block scope
+{
+    let a=10;
+    console.log(a);
+}
+{
+    let a=15;
+    console.log(a);
+}
+{
+    const b=20;
+    console.log(b);
+}
+{
+    const b=25;
+    console.log(b);
+}
