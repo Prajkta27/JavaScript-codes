adb11bd0f290e6f0f86fb3a4733ac8345dda8520
Prajkta Gudle <124344569+Prajkta27@users.noreply.github.com>
2024-05-22 14:24:27 +0530

    First.js

--- First.js
+++ First.js
@@ -15,3 +15,29 @@
     const b=25;
     console.log(b);
 }
+//For objrct creation use following sysntax
+//object is declared using key value pair
+//it is declared using let/const same as variable
+//we can change the key assigned wihin the object even if it's const. Ex: we can change name from Student object
+
+const Student=
+{
+    name:"Prajkta",
+    age:23,
+    cgpa:9.3,
+    isPass: true
+};
+console.log(typeof(Student));       //object
+
+//To access particular in object use:
+Student["name"];       //Prajkta
+//or we can also use
+Student.name;          //Prajkta
+
+//if we want to increase some key avlue in object
+Student["age"]=Student["age"]+1;        
+console.log(Student.age);         //24
+
+//to change value of the key within object
+Student["name"]="Pradyumn";
+console.log(Student.name);        //Pradyumn
