17075fceaecb4039f60aa3c316b2bee700226c1e
Prajkta Gudle <124344569+Prajkta27@users.noreply.github.com>
2024-05-23 17:23:41 +0530

    First.js

--- First.js
+++ First.js
@@ -152,3 +152,38 @@ console.log(colour);
 let age=25;
 let result= age >= 18 ? "adult": "not adult";
 console.log(result);                  //adult
+
+//Practice question using prompt
+//Take a number from user using prompt and check whether that number is multiple of 5 or not
+let num= prompt("Enter a number");
+console.log(num);
+if(num % 5 === 0)
+    {
+        console.log(num, "is a multiple of 5");
+    }
+else
+{
+    console.log(num, "is not a multiple of 5");
+}
+
+//Write a code which can give grades to students according to their scores
+//90-100, A      70-89, B       60-69, C        50-59, D        0-49, F
+let score= prompt("Enter a score");
+let grade;
+
+if(score>=90 && score<=100)
+    {
+        grade="A";
+    }
+    else if(score>=70 && score<=89){
+        grade="B";
+    }
+    else if(score>=60 && score<=69){
+            grade="C";
+        }
+    else if(score>=50 && score<=59){
+        grade="D";
+    }else{
+        grade="F"
+    }
+    console.log(grade);
