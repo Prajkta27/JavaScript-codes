607a9445ec08c40a5fbc67e7c03cb21557b8065e
Prajkta Gudle <124344569+Prajkta27@users.noreply.github.com>
2024-05-23 17:27:13 +0530

    First.js

--- First.js
+++ First.js
@@ -168,7 +168,7 @@ else
 
 //Write a code which can give grades to students according to their scores
 //90-100, A      70-89, B       60-69, C        50-59, D        0-49, F
-let score= prompt("Enter a score");
+let score= prompt("Enter a score(0-100):");
 let grade;
 
 if(score>=90 && score<=100)
