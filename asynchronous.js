57e7d80a1569a948dae38555c65fdbf499e87a21
Prajkta Gudle <124344569+Prajkta27@users.noreply.github.com>
2024-06-03 16:19:27 +0530

    asynchronous.js

--- asynchronous.js
+++ asynchronous.js
@@ -3,16 +3,16 @@
   //syanchronous: code will execute in the way we assign or write them (Examples till now)
 
   //Asynchronous: works on setTimeout()
-  /*console.log("one");
+  console.log("one");
   console.log("two");
   setTimeout(() =>{
     console.log("hello")
   }, 4000)
   console.log("three");
-  console.log("four"); */       //this will print one, two three four at start & hello after 4 sec (4 sec = 4000 ms)
+  console.log("four");       //this will print one, two three four at start & hello after 4 sec (4 sec = 4000 ms)
 
   //callback: a functiom is passes as an argumnet of another function
-  //nestyed callbacks
+  //nested callbacks
   function getData(getID, getNextData) {
     setTimeout(() => {
         console.log("Data:", getID);
